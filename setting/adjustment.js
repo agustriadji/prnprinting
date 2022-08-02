c34bdea671afde7d1ddb917460bdd0d9f032dd11
magictree <agusmemons90@gmail.com>
2022-08-02 20:49:28 +0700

    add direct module printer

--- setting/adjustment.js
+++ setting/adjustment.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path")
 
 let DEFAULT =  path.resolve(__dirname,"../content/default.txt");
 let SETTING = path.resolve(__dirname,"../content/settingDefault.json");
